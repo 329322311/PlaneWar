5f0f93ad099e9f3703fccae06349c7ef5afdc222
Jayson chen <329322311@qq.com>
2018-12-07 16:43:30 +0800

    Add files via upload

---
+++ map.py
@@ -0,0 +1,39 @@
+# coding="utf-8"
+# author : "Jayson"
+# map module
+
+# zh-cn:导入pygame模块
+# en:import the pygame module
+import pygame
+
+WIN_WIDTH = 512
+WIN_HEIGHT = 768
+map_move_speed = 2
+
+class Map(object):
+
+    def __init__(self,win):
+        # 加载两张背景图片;load two background images
+        self.img = pygame.image.load("res/img_bg_level_1.jpg")
+        self.img2 = pygame.image.load("res/img_bg_level_1.jpg")
+
+        # 获取窗口对象；get window object
+        self.window = win
+
+        # 设置两张图片的y值；set the y values for both images
+        self.y = 0
+        self.y2 = -WIN_HEIGHT
+
+    def move(self):
+        # 地图的移动；set map movement
+        self.y += map_move_speed
+        self.y2 += map_move_speed
+        if self.y > WIN_HEIGHT:
+            self.y = -WIN_HEIGHT
+        if self.y2 > WIN_HEIGHT:
+            self.y2 = -WIN_HEIGHT
+
+    def blited(self):   # 绘制背景；draw background into window
+        self.window.blit(self.img,(0,self.y))
+        self.window.blit(self.img2,(0, self.y2))
+

1c1cb429111cc2af0a907205e2506a5cacf21624
Jayson chen <329322311@qq.com>
2018-12-08 23:05:05 +0800

    Create setting.py

---
+++ setting.py
@@ -0,0 +1,11 @@
+# coding="utf-8"
+# author : "Jayson"
+# setting module
+
+# 储存游戏设置;save game Settings
+WIN_WIDTH = 512
+WIN_HEIGHT = 768
+
+map_move_speed = 2
+bullet_move_speed = 4
+plane_move_speed = 3

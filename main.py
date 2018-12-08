b38dc4420d0b61a8f8a1084c5cf255a6856c9aab
Jayson chen <329322311@qq.com>
2018-12-08 23:05:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 # zh-cn:导入pygame,sys模块
 # en:import the pygame,sys module
 
-import pygame,sys
+import pygame,sys,setting
 
 # 创建飞机大战的大类
 # Create class of PlaneWar
@@ -12,8 +12,6 @@ from enemy import Enemy
 from hero import Hero
 from map import Map
 
-WIN_WIDTH = 512
-WIN_HEIGHT = 768
 
 class PlaneWar(object):
 
@@ -24,7 +22,7 @@ class PlaneWar(object):
         pygame.init()
 
         # 创建窗口;set window sizes
-        window = pygame.display.set_mode([WIN_WIDTH, WIN_HEIGHT])
+        window = pygame.display.set_mode([setting.WIN_WIDTH, setting.WIN_HEIGHT])
 
         # 调用设置窗口;run setting window properties
         self.set_window()
@@ -78,6 +76,14 @@ class PlaneWar(object):
                 if pygame.Rect.colliderect(i.bullet_rect, j.enemy_rect):
                     j.enemy_reset()
 
+    def crash(self):
+        # 判断两个矩形是否相交，相交返回True，否则返回False;to determine whether two rectangles intersect, the intersection returns True or False
+        for i in self.enemy:
+                if pygame.Rect.colliderect(i.enemy_rect, self.hero.hero_rect):
+                    return True
+        else:
+            return False
+
     # 定义绘制方法；define the drawing method
     def blit(self):
         self.map.blited()
@@ -91,6 +97,17 @@ class PlaneWar(object):
     def display(self):
         pygame.display.update()
 
+    def gameover(self):
+        while True:
+            # 获取响应事件;get the events in response
+            event_list = pygame.event.get()
+            # 遍历事件;traversal events
+            for event in event_list:
+                # 鼠标点击关闭窗口事件（解决未响应）;exit event
+                if event.type == pygame.QUIT:
+                    print("关闭了窗口")
+                    sys.exit()
+
     # 定义运行方法；define the operation method
     def run(self):
         while True:
@@ -101,10 +118,13 @@ class PlaneWar(object):
             self.move()
             # 3.判断是否打中敌机;judge whether you hit the enemys
             self.hit_enemy()
+            if self.crash():
+                break
             # 4.绘制图片对象；run a bullet method
             self.blit()
             # 5.更新界面；update display content
             self.display()
+        self.gameover()
 
 if __name__ =="__main__":
     # 创建game对象；Create game object

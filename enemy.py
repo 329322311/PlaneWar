2e58ebfe01694cf67e4c90dfb1817f51b97d3981
Jayson chen <329322311@qq.com>
2018-12-08 23:07:01 +0800

    Update enemy.py

--- enemy.py
+++ enemy.py
@@ -5,10 +5,8 @@
 # zh-cn:导入pygame模块
 # en:import the pygame module
 import pygame
-import random
+import random,setting
 
-WIN_WIDTH = 512
-WIN_HEIGHT = 768
 
 
 class Enemy(object):
@@ -25,7 +23,7 @@ class Enemy(object):
 
         # 设置矩形的位置; set the rectangle's position
         self.enemy_rect[1] = random.randint(-300,0)
-        self.enemy_rect[0] = random.randint(0,WIN_WIDTH-self.enemy_rect[2])
+        self.enemy_rect[0] = random.randint(0,setting.WIN_WIDTH-self.enemy_rect[2])
 
         self.speed = random.randint(30, 40) * 0.1
 
@@ -33,14 +31,14 @@ class Enemy(object):
         self.enemy_rect[1] += self.speed
 
         # 当飞机超过窗口时，飞机重置;the enemy resets when it passed the window
-        if self.enemy_rect[1] > WIN_HEIGHT:
+        if self.enemy_rect[1] > setting.WIN_HEIGHT:
             self.enemy_reset()
 
     # 重置方法;reset method
     def enemy_reset(self):
         self.img = pygame.image.load("res/img-plane_%s.png" % random.randint(1, 7))
         self.enemy_rect[1] = random.randint(-300, 0)
-        self.enemy_rect[0] = random.randint(0, WIN_WIDTH - self.enemy_rect[2])
+        self.enemy_rect[0] = random.randint(0, setting.WIN_WIDTH - self.enemy_rect[2])
         self.speed = random.randint(30,45) * 0.1
 
     # 绘制背景；draw background into window

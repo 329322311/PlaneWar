e60bd15885948e4149f9ecf604a4bc1942176f69
Jayson chen <329322311@qq.com>
2018-12-08 17:49:12 +0800

    Update enemy.py

--- enemy.py
+++ enemy.py
@@ -8,6 +8,8 @@ import pygame
 import random
 
 WIN_WIDTH = 512
+WIN_HEIGHT = 768
+
 
 class Enemy(object):
 
@@ -22,11 +24,24 @@ class Enemy(object):
         self.enemy_rect = self.img.get_rect()
 
         # 设置矩形的位置; set the rectangle's position
-        self.enemy_rect[1] = 0
+        self.enemy_rect[1] = random.randint(-300,0)
         self.enemy_rect[0] = random.randint(0,WIN_WIDTH-self.enemy_rect[2])
 
+        self.speed = random.randint(30, 40) * 0.1
+
     def move(self):
-        pass
+        self.enemy_rect[1] += self.speed
+
+        # 当飞机超过窗口时，飞机重置;the enemy resets when it passed the window
+        if self.enemy_rect[1] > WIN_HEIGHT:
+            self.enemy_reset()
+
+    # 重置方法;reset method
+    def enemy_reset(self):
+        self.img = pygame.image.load("res/img-plane_%s.png" % random.randint(1, 7))
+        self.enemy_rect[1] = random.randint(-300, 0)
+        self.enemy_rect[0] = random.randint(0, WIN_WIDTH - self.enemy_rect[2])
+        self.speed = random.randint(30,45) * 0.1
 
     # 绘制背景；draw background into window
     def blited(self):
